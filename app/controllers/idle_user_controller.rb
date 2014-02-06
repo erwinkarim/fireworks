e9c49c3b47a63252742f87b5dff9292513038b9e
erwinkarim <erwinkarim@gmail.com>
2014-02-06 12:02:24 +0800

    fix param issue

--- app/controllers/idle_user_controller.rb
+++ app/controllers/idle_user_controller.rb
@@ -4,25 +4,25 @@ class IdleUserController < ApplicationController
     
     if (params.has_key?(:user) && params.has_key?(:host) && params.has_key?(:idle)) then
      
-      puts "Getting idle client updates from "+params[:user]+"@"+params[:host] 
+      puts "Getting idle client updates from "+ params[:user]+"@"+ params[:host] 
       @idleuser = IdleUser.where("user = ? and hostname = ?", params[:user], params[:host]).first 
       if @idleuser.nil? then
         @idleEntry = IdleUser.new(:user => params[:user], :hostname => params[:host], 
           :idle => params[:idle])
         if @idleEntry.save then
           render :text => 'new entry created'
-          puts "Created new entry for "+params[:user] +"@"+param[:host]
+          puts "Created new entry for "+ params[:user] +"@" + param[:host]
         else
           render :text => 'failed to create entry'
-          puts "Failed to Create entry for "+ params[:user] + "@"+params[:host]
+          puts "Failed to Create entry for "+ params[:user] + "@"+ params[:host]
         end
       else
         if @idleuser.update_attribute(:idle, params[:idle]) then
           render :text => 'update user '+ params[:user] +"@" + params[:host] + " with idle time:" +  params[:idle]
-          puts "Successfully updates "+params[:user]+"@"+params[:host]
+          puts "Successfully updates "+ params[:user]+"@"+ params[:host]
         else
           render :text => 'Fail to update user'
-          puts "Update "+params[:user]+"@"+params[:host]+" failed"
+          puts "Update "+ params[:user]+"@"+ params[:host]+" failed"
         end
       end
     else
