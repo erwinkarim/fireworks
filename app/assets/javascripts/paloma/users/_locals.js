fd028797e1baa1e83240f674106312e1480d7f08
erwinkarim <erwinkarim@gmail.com>
2014-03-12 10:20:20 +0800

    issue #1: user usage graph load 1000 points at a time. further refinement required to add new series as you lazy load

--- app/assets/javascripts/paloma/users/_locals.js
+++ app/assets/javascripts/paloma/users/_locals.js
@@ -22,17 +22,54 @@
 
       //show the thing is being loaded
 
+      var data_load_path = '/users/' + e.target.attributes['data-user-id'].value + 
+        '/machines/' + e.target.attributes['data-machine-id'].value + '/gen_features';
+
       //load the chart
-      $.ajax( 
-        '/users/' + e.target.attributes['data-user-id'].value + 
-        '/machines/' + e.target.attributes['data-machine-id'].value + '/gen_features', {
+      $.ajax( data_load_path, {
         dataType:'json'
       }).done( function(data, textStatus, jqXHR){
-        console.log(data);
         $('.tab-pane[data-machine-id="' + e.target.attributes['data-machine-id'].value + 
           '"][data-user-id="' + e.target.attributes['data-user-id'].value + '"]').highcharts('StockChart', {
           //setup the chart
           title: { text: 'Features usage by the user on machine' },
+          chart: {
+            events : {
+              load: function(){
+                var chart_handle = this;
+                function recursive_data_load(last_data_point){
+                  //only call ajax if there's more data to load in the graph
+                  if(last_data_point != 0){
+                    $.ajax( data_load_path, {
+                      dataType:'json',
+                      data:{ start_id:last_data_point}
+                    }).done( function(data, textStatus, jqXHR) {
+                      console.log('chart loaded, lazyly add more data points from features.id ' + data['last_data_point']);
+                      if( data['graph_data'].length != 0){
+                        //load data from matched series, otherwise add new series to the graph
+                        $.each( data['graph_data'], function( index, value){
+                          $.each( chart_handle.series, function( chart_index, serie){
+                            if( serie.name == value['name']) {
+                              $.each( value['data'], function( data_index, data_value ) {
+                                serie.addPoint( data_value , false, false);
+                              });
+                            }
+                          });
+                        });
+                        chart_handle.redraw();
+                      };
+                      recursive_data_load(data['last_data_point']);
+                    });
+                  }
+                };
+  
+                recursive_data_load(data['last_data_point']);
+              },
+              click: function(e){
+                console.log(e);
+              }
+            }
+          },
           rangeSelector: {
             buttons: [
               { type: 'hour', count: 1, text: '1h' }, 
@@ -43,7 +80,7 @@
               { type: 'all', text: 'All' }
             ], selected : 2 // all
           }, 
-          series: data
+          series: data['graph_data']
         });
       }); // $.ajax( 
 
--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -2,16 +2,38 @@ class MachinesController < ApplicationController
   
   # GET    /users/:user_id/machines/:id/gen_features(.:format)
   # get the features of this machine that the users 
+  # retures what features that this machines has used 
+  # options:-
+  #   start_id   start from this id and descend 1000 data points
+  # in json mode, returns the last 1000 data points in this format:-
+  #     [ {:name => feature_name, :data => [ [x1,y1], [x2,y2],...,[xn,yn]] } ] 
+  #     should return
+  #     { 
+  #       :last_data_point => lowest features id,
+  #       :graph_data => [ 
+  #           { :name => feature_name1, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] }, 
+  #           ...
+  #           { :name => feature_nameN, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] }, 
+  #       ]
   def gen_features
     @user = User.find(params[:user_id])
     @machine = @user.machines.find(params[:id])
-    @features = @machine.features
+    if params.has_key? :start_id then
+      start_id = params[:start_id].to_i - 1
+    else
+      start_id = Feature.last.id
+    end
+    @features = @machine.features.where{ (id.lteq start_id) }.order('features.id desc').limit(1000)
+    #@features = @machine.features.where{ (id.lteq start_id) }.order('features.created_at asc')
 
     respond_to do |format|
       format.json { 
         init_hash = @features.select(:name).inject({}){ |m,e| m.merge( { e.name.to_sym => [] } ) }
         @features.order(:created_at).each{ |x| init_hash[x.name.to_sym] << [x.created_at.to_i * 1000 , x.current] } 
-        render :json => init_hash.inject([]){ |m,e| m << { :name => e[0], :data => e[1] } }
+        render :json => {
+          :last_data_point => @features.empty? ? 0 : @features.min.id, 
+          :graph_data => init_hash.inject([]){ |m,e| m << { :name => e[0], :data => e[1].sort } }
+        }
       }
       format.html { 
         render :partial => 'features_accordion', 
