2bdde43378b76f7f6b5f825cf46bfc5c9bf87bea
erwinkarim <erwinkarim@gmail.com>
2014-08-26 16:14:48 +0800

    #7: added new page for tags permalink; #8: failed to load features page will show notice that something went wrong

--- app/assets/javascripts/paloma/tags/index.js
+++ app/assets/javascripts/paloma/tags/index.js
@@ -18,122 +18,6 @@
 
 
   Paloma.callbacks['tags']['index'] = function(params){
-    function setup_features_accordion(handle){
-      handle.find('.accordion-body').on('show', function(){
-        //load the graph
-        if(handle.find('.features-graph').children().length == 0){
-          var data_load_path = '/licservers/' + handle.attr('data-licserver') + '/features/' + 
-            handle.attr('data-feature') + '/get_data';
-          handle.find('.features-graph').highcharts('StockChart', {
-            title: { text: handle.attr('data-feature') + ' Usage' },
-            chart: {
-              events: {
-                load: function(){
-                  //lazy load the data here
-                  var chart_handle = this;
-                  function recursive_data_load(last_data_point){
-                    if(last_data_point != 0 || last_data_point == null){
-                      chart_handle.showLoading();
-                      if(last_data_point == null){
-                        var data_header = {};
-                      } else {
-                        var data_header = { start_id:last_data_point };
-                      }
-                      $.ajax( data_load_path, {
-                        dataType:'json', data:data_header
-                      }).done( function(data, textStatus, jqXHR){
-                        console.log('load data into the graph upto id ' + data['last_id'] )
-                  
-                        //add current data
-                        //better wayt load 10000 data points at a time
-                        for(i=0; i < data['data'][0]['data'].length; i++){
-                          chart_handle.series[0].addPoint( data['data'][0]['data'][i], false, false );
-                          chart_handle.series[1].addPoint( data['data'][1]['data'][i], false, false );
-                        }
-
-                        //only draw for the first time
-                        if(last_data_point == null){
-                          chart_handle.redraw();
-                          chart_handle.hideLoading();
-                        };
-
-                        //for now it's slow and lock up the browser
-                        //recursive_data_load(data['last_id']);
-                      });
-                    }
-                  }
-
-                  recursive_data_load(null);
-                  chart_handle.redraw();
-                }
-              }
-            },
-            rangeSelector: {
-              buttons: [
-                { type: 'hour', count: 1, text: '1h' }, 
-                { type: 'day', count: 1, text: '1d' }, 
-                { type: 'week', count: 1, text: '1w' }, 
-                { type: 'month', count: 1, text: '1m' }, 
-                { type: 'year', count: 1, text: '1y' }, 
-                { type: 'all', text: 'All' }
-              ], selected : 2 // all
-            } ,
-            series:[
-              { name:'current', data:[ ] },
-              { name:'max' , data:[ ]}
-            ],
-            plotOptions: {
-            }
-          }); // handle.find('.features-graph').highcharts('StockChart', {
-            
-        }
-      });
-      handle.removeAttr('data-init-feature');
-      
-    };
-
-    //setup the licserver tabs
-    function setup_tab(handle){
-      handle.on('shown', function(e){
-        //when the licserver is clicked, show licserver info and detected features
-        $.get('/licservers/' + e.target.attributes['data-licserver'].value + '.template', null, 
-          function(data, textStatus, jqXHR){
-            if(
-              $(
-                '#' + e.target.attributes['data-tag'].value + '-' + e.target.attributes['data-licserver'].value
-              ).children().length == 0
-            ){
-              $('#' + e.target.attributes['data-tag'].value + '-' + e.target.attributes['data-licserver'].value).append(
-                data
-              ).ready( function(){
-                //load the featres listings
-                $.get('/licservers/' + e.target.attributes['data-licserver'].value + '/features/list', null, 
-                  function(data, textStatus, jqXHR){
-                    $('#licserver-' + e.target.attributes['data-licserver'].value + '-features').find('.fa-spinner').remove();
-                    $('#licserver-' + e.target.attributes['data-licserver'].value + '-features-listing').append(
-                      data
-                    ).ready( function(){
-                      //when a feature is selected, show it's usage over time. lazy load the data
-                      $('.accordion-group[data-init-feature="false"]').each( function(index,value){
-                        setup_features_accordion($(this));
-                      });
-
-
-                    });
-                  }, 'html' ).fail( function(){
-										console.log('fail to load server');
-                    $('#licserver-' + e.target.attributes['data-licserver'].value + '-features').find('.fa-spinner').remove();
-                    $('#licserver-' + e.target.attributes['data-licserver'].value + '-features-listing').append(
-											$.parseHTML('Opss, something went wrong. Failed to load feature listings')
-										);
-									}); //$.get('/licservers/' + e.target.attributes['data-licserver'].value + '/features/list', null, 
-              });
-            };
-          }, 'html' );
-      });
-      handle.removeAttr('data-setup');
-
-    };
 
     //setup the accordion
     function setup_accordion( handle ){
@@ -144,7 +28,8 @@
             handle.find('.accordion-inner').append(data).ready( function(){
               //when the licserver is clicked, show licserver info and detected features
               $('a[data-toggle="tab"][data-init="false"]').each( function(index, value){
-                setup_tab($(this));
+								_l.setup_tab($(this));
+                //setup_tab($(this));
               });
 
               
@@ -152,21 +37,6 @@
           }, 'html' );
         }
       });
-    };
-
-    function init_load(){
-      //load all them tags
-      $.get( '/tags/gen_accordion', null,
-        function(data, textStatus, jqXHR){
-          $('#tags-accordion').append(data).ready( function(){
-            //process the accordion
-            $('.accordion-tag').each( function(index, value) {
-              setup_accordion( $(this) );
-            });
-          }) 
-        }, 'html'
-      );
-
     };
 
     // Do something here.
@@ -178,7 +48,18 @@
         }
       });
     
-      init_load();
+      //init_load();
+      //load all them tags
+      $.get( '/tags/gen_accordion', null,
+        function(data, textStatus, jqXHR){
+          $('#tags-accordion').empty().append(data).ready( function(){
+            //process the accordion
+            $('.accordion-tag').each( function(index, value) {
+              setup_accordion( $(this) );
+            });
+          }) 
+        }, 'html'
+      );
 
       //setup search query
       $('#search-tags').typeahead({
---
+++ app/assets/javascripts/paloma/tags/show.js
@@ -0,0 +1,30 @@
+(function(){
+  // You access variables from before/around filters from _x object.
+  // You can also share variables to after/around filters through _x object.
+  var _x = Paloma.variableContainer;
+
+  // We are using _L as an alias for the locals container.
+  // Use either of the two to access locals from other scopes.
+  //
+  // Example:
+  // _L.otherController.localVariable = 100;
+  var _L = Paloma.locals;
+
+  // Access locals for the current scope through the _l object.
+  //
+  // Example:
+  // _l.localMethod(); 
+  var _l = _L['tags'];
+
+
+  Paloma.callbacks['tags']['show'] = function(params){
+    // Do something here.
+    $(document).ready( function(){
+			$('.nav').find('a[data-toggle="tab"]').each( function(index, value){
+				_l.setup_tab($(this));
+			});
+			$('.nav').find('a[data-toggle="tab"]:first').click();
+
+		});
+  }; // Paloma.callbacks['tags']['show'] = function(params){
+})();
