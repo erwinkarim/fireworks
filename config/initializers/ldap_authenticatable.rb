bf54d4332fab8fc1e44062696949e259aee175ab
erwinkarim <erwinkarim@gmail.com>
2014-12-29 13:33:01 +0800

    #12 #13: add group membership test when loggin in. prevent unauthorized users (not member of group) from loggin in

--- config/initializers/ldap_authenticatable.rb
+++ config/initializers/ldap_authenticatable.rb
@@ -11,22 +11,33 @@ module Devise
 					ldap.port = 636
 					ldap.base = ENV['devise_ldap_base']
 					ldap.encryption :simple_tls
-					ldap.auth "PETRONAS\\#{login}", password
+					ldap.auth "#{login}@#{params[:ads_user][:domain]}", password
+
+					valid_login = false
+					in_group = false
 						
 					if ldap.bind
-						Rails.logger.info "login successful"
+						valid_login = true
 		
 						#find the user
 						filter = Net::LDAP::Filter.eq( 'samaccountname', params[:ads_user][:username] )
 						search_result = ldap.search( :base => ENV['devise_ldap_base'], :filter => filter).first
 
 						#check if the user is in the proper group
-						if ENV['devise_check_group'] then
+						if ENV['devise_check_group'] == 'true' then
+							group_search_results = ldap.search( :base => search_result[:dn].first, 
+								:filter => Net::LDAP::Filter.ex( "memberof:1.2.840.113556.1.4.1941", ENV['devise_req_groups']),
+								:scope => Net::LDAP::SearchScope_BaseObject)
+							if group_search_results.length == 1 then
+								in_group = true
+							end
 						else
 							in_group = true
 						end	
+					end
 
-						#ads_user = AdsUser.find_or_create_by(login:params[:ads_user][:username])
+					if valid_login && in_group then
+						#start looking for user or create a new one
 						ads_user = AdsUser.where(:login => params[:ads_user][:username]).first
 
 						if ads_user.nil? then
@@ -36,7 +47,6 @@ module Devise
 								:username => search_result[:samaccountname].first, 
 								:password => params[:ads_user][:password], :domain => params[:ads_user][:domain] )
 							ads_user.save!
-							Rails.logger.info "ads_user is #{ads_user.inspect}"
 							params[:ads_user][:email] = search_result[:mail].first
 						else
 							ads_user.update_attributes({ :email => search_result[:mail].first, 
@@ -46,8 +56,9 @@ module Devise
 
 						success!(ads_user)
 					else
-						Rails.logger.info "login fail as #{login}"
-						fail(:invalid_login)
+						#somehow this doesn't work if the user have logon before so blank out username field
+						params[:ads_user][:username] = ""
+						return fail(:invalid)
 					end
 				end
 			end
