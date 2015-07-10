79fb41d12f8f1259f92d6462a76a219fc60c2418
erwinkarim <erwinkarim@gmail.com>
2015-07-10 15:47:11 +0800

    #31: user feature usage graph loads a lot faster, but locks up the machine and new series are being created. memory usage is also high

--- app/assets/javascripts/paloma/users/_locals.js
+++ app/assets/javascripts/paloma/users/_locals.js
@@ -9,7 +9,7 @@
   // locals.localMethod = function(){};
   var locals = Paloma.locals['users'] = {};
 
-  
+
   // ~> Start local definitions here and remove this line.
 	/* setup the tabs
 	 * handles must have these stucture:-
@@ -17,16 +17,16 @@
 			%ul.nav.nav-tabs{ :id => 'user' + @user.id.to_s + '-tabs' }
 				- @user.machines.each do |machine|
 					%li
-						%a{ :href => '#user' + @user.id.to_s + '-' + 'machine' + machine.id.to_s, 
+						%a{ :href => '#user' + @user.id.to_s + '-' + 'machine' + machine.id.to_s,
 							:data => { :toggle => 'tab', :'user-id' => @user.id, :'machine-id' => machine.id } }
 							= machine.name
 			.tab-content
 				- @user.machines.each do |machine|
-					.tab-pane{ :id => 'user' + @user.id.to_s + '-' + 'machine' + machine.id.to_s, 
+					.tab-pane{ :id => 'user' + @user.id.to_s + '-' + 'machine' + machine.id.to_s,
 						:data => { :'machine-id' => machine.id, :'user-id' => @user.id } }
-	 * 			
 	 *
-	*/ 	
+	 *
+	*/
 	//setup accordion body
   locals.setup_accordion_body = function( handle) {
     handle.find('a[data-toggle="tab"]').on('shown.bs.tab', function(e){
@@ -34,7 +34,7 @@
 			/*
 			var anchor_handle = handle.find('.nav-tabs').find(
 				'a[data-machine-id="' + e.target.attributes['data-machine-id'] + '"][data-user-id="' + e.target.attributes['data-user-id'] + '"]'
-				); 
+				);
 			*/
 			var anchor_handle = handle.find('.nav-tabs').find( "a[data-machine-id='" + e.target.attributes['data-machine-id'].value + "']" );
 			if(anchor_handle.attr('data-init') != 'false' ){
@@ -43,17 +43,17 @@
 			};
 
       //when clicked, start gather features data
-      $('.tab-pane[data-machine-id="' + e.target.attributes['data-machine-id'].value + 
+      $('.tab-pane[data-machine-id="' + e.target.attributes['data-machine-id'].value +
         '"][data-user-id="' + e.target.attributes['data-user-id'].value + '"]').append(
         $.parseHTML('<i class="fa fa-cog fa-spin fa-4x"></i>')
       );
 
       //show the thing is being loaded
 
-      var data_load_path = '/users/' + e.target.attributes['data-user-id'].value + 
+      var data_load_path = '/users/' + e.target.attributes['data-user-id'].value +
         '/machines/' + e.target.attributes['data-machine-id'].value + '/gen_features';
 
-			var tab_handle = handle.find('.tab-pane[data-machine-id="' + e.target.attributes['data-machine-id'].value + 
+			var tab_handle = handle.find('.tab-pane[data-machine-id="' + e.target.attributes['data-machine-id'].value +
           '"][data-user-id="' + e.target.attributes['data-user-id'].value + '"]');
       //load the chart
       $.ajax( data_load_path, {
@@ -66,9 +66,11 @@
             events : {
               load: function(){
                 var chart_handle = this;
-                function recursive_data_load(last_data_point){
+                var cycles = 50;
+                function recursive_data_load(last_data_point, countdown){
                   //only call ajax if there's more data to load in the graph
-                  if(last_data_point != 0){
+                  // this locks up browser as it adds new series in a rapid manner
+                  if(last_data_point != 0 && countdown != 0){
                     $.ajax( data_load_path, {
                       dataType:'json',
                       data:{ start_id:last_data_point}
@@ -94,12 +96,11 @@
                         });
                         chart_handle.redraw();
                       };
-                      recursive_data_load(data['last_data_point']);
+                      recursive_data_load(data['last_data_point'], countdown - 1);
                     });
                   }
-                };
-  
-                recursive_data_load(data['last_data_point']);
+                } ;
+                recursive_data_load(data['last_data_point'], cycles);
               },
               click: function(e){
                 console.log(e);
@@ -108,22 +109,22 @@
           },
           rangeSelector: {
             buttons: [
-              { type: 'hour', count: 1, text: '1h' }, 
-              { type: 'day', count: 1, text: '1d' }, 
-              { type: 'week', count: 1, text: '1w' }, 
-              { type: 'month', count: 1, text: '1m' }, 
-              { type: 'year', count: 1, text: '1y' }, 
+              { type: 'hour', count: 1, text: '1h' },
+              { type: 'day', count: 1, text: '1d' },
+              { type: 'week', count: 1, text: '1w' },
+              { type: 'month', count: 1, text: '1m' },
+              { type: 'year', count: 1, text: '1y' },
               { type: 'all', text: 'All' }
             ], selected : 2 // all
-          }, 
+          },
           series: data['graph_data']
         });
       }).fail( function(jqXHR, textStatus, errorThrown){
 				tab_handle.empty().append(
 					'failed to load: ' + textStatus
 				);
-					
-			}); // $.ajax( 
+
+			}); // $.ajax(
 
       //remove the spinner
 			anchor_handle.attr('data-init', 'true' );
--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,42 +1,47 @@
 class MachinesController < ApplicationController
-  
+
   # GET    /users/:user_id/machines/:id/gen_features(.:format)
-  # get the features of this machine that the users 
-  # retures what features that this machines has used 
+  # get the features of this machine that the users
+  # retures what features that this machines has used
   # options:-
   #   start_id   start from this id and descend 1000 data points
   # in json mode, returns the last 1000 data points in this format:-
-  #     [ {:name => feature_name, :data => [ [x1,y1], [x2,y2],...,[xn,yn]] } ] 
+  #     [ {:name => feature_name, :data => [ [x1,y1], [x2,y2],...,[xn,yn]] } ]
   #     should return
-  #     { 
+  #     {
   #       :last_data_point => lowest features id,
-  #       :graph_data => [ 
-  #           { :name => feature_name1, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] }, 
+  #       :graph_data => [
+  #           { :name => feature_name1, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] },
   #           ...
-  #           { :name => feature_nameN, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] }, 
+  #           { :name => feature_nameN, :data => [ [x1,y1], [x2,y2], .. [xn,yn] ] },
   #       ]
   def gen_features
     @user = User.find(params[:user_id])
     @machine = @user.machines.find(params[:id])
+    theMachineId = @machine.id
     if params.has_key? :start_id then
       start_id = params[:start_id].to_i - 1
     else
       start_id = Feature.last.id
     end
-    @features = @machine.features.where{ (id.lteq start_id) }.order('features.id desc').limit(1000)
-    #@features = @machine.features.where{ (id.lteq start_id) }.order('features.created_at asc')
+    #@features = @machine.features.where{ (id.lteq start_id) }.order('features.id desc').limit(100)
+    #suppose to be much faster
+    @features = Feature.where(:id =>
+      MachineFeature.where{
+        (machine_id.eq theMachineId) && ( feature_id.lteq start_id ) }.limit(1000).order(:feature_id => :desc).pluck(:feature_id)
+    )
 
     respond_to do |format|
-      format.json { 
+      format.json {
         init_hash = @features.select(:name).inject({}){ |m,e| m.merge( { e.name.to_sym => [] } ) }
-        @features.order(:created_at).each{ |x| init_hash[x.name.to_sym] << [x.created_at.to_i * 1000 , x.current] } 
+        @features.order(:created_at).each{ |x| init_hash[x.name.to_sym] << [x.created_at.to_i * 1000 , x.current] }
         render :json => {
-          :last_data_point => @features.empty? ? 0 : @features.min.id, 
+          :last_data_point => @features.empty? ? 0 : @features.min.id,
           :graph_data => init_hash.inject([]){ |m,e| m << { :name => e[0], :data => e[1].sort } }
         }
       }
-      format.html { 
-        render :partial => 'features_accordion', 
+      format.html {
+        render :partial => 'features_accordion',
           :locals => { :user => @user, :machine => @machine, :features => @features }
       }
     end
