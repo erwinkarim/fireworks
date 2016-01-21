013e7f5b4d220e3187506114ac328d91bd1ef07f
unknown <erwinkarim@gmail.com>
2016-01-21 14:23:30 +0800

    #43 add login pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	after_filter :store_location
 	def store_location
 		# store last url - this is needed for post-login redirect to whatever the user last visited.
-		return unless request.get? 
+		return unless request.get?
 		if (request.path != "/ads_users/sign_in" &&
 			request.path != "/ads_users/sign_up" &&
 			request.path != "/ads_users/password/new" &&
@@ -12,19 +12,19 @@ class ApplicationController < ActionController::Base
 			request.path != "/ads_users/confirmation" &&
 			request.path != "/ads_users/sign_out" &&
 			!request.xhr?) # don't store ajax calls
-			session[:previous_url] = request.fullpath 
+			session[:previous_url] = request.fullpath
 		end
 	end
 
 	def after_sign_in_path_for(resource)
-		#session[:previous_url] || root_path
-		ads_user_watch_lists_path(current_ads_user.login)
+		session[:previous_url] || root_path
+		#ads_user_watch_lists_path(current_ads_user.login)
 	end
 
 	protected
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_in ) do |username|
-		end 
+		end
 	end
 
   #rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
